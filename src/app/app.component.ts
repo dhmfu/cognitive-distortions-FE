21d1d8bc4c1af01d2131dbb04f567eaf342618cc
Vasyl Dudka <dudka.vasia2010@gmail.com>
2024-12-01 01:03:24 +0200

    add material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,19 @@
 import { Component, ElementRef, viewChild } from '@angular/core';
 import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { MatButton, MatButtonModule } from '@angular/material/button';
 import { RouterOutlet } from '@angular/router';
 import { filter, first, fromEvent, map, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'username';
 
-  btnRef = viewChild<ElementRef<HTMLButtonElement>>('btn');
+  btnRef = viewChild<MatButton, ElementRef<HTMLButtonElement>>('btn', { read: ElementRef });
   dominanceAsserted = this.getData();
 
   private getData() {
