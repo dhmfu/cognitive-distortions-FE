5cda2ba243e12658b2cfa9155d8cf539d5cbdaa2
Vasyl Dudka <dudka.vasia2010@gmail.com>
2024-12-01 00:37:32 +0200

    add PWA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, viewChild } from '@angular/core';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 import { RouterOutlet } from '@angular/router';
+import { filter, first, fromEvent, map, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +10,19 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'congitive-distortions-FE';
+  title = 'username';
+
+  btnRef = viewChild<ElementRef<HTMLButtonElement>>('btn');
+  dominanceAsserted = this.getData();
+
+  private getData() {
+    const dataSource = toObservable(this.btnRef).pipe(
+      filter(ref => !!ref),
+      switchMap((ref) => fromEvent(ref.nativeElement, 'click')),
+      first(),
+      map(() => 'TRUE' as 'TRUE' | 'FALSE')
+    );
+
+    return toSignal(dataSource, { initialValue: 'FALSE' });
+  }
 }
