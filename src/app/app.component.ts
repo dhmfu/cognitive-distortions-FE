47116feb95c63fb1bb27e5b6185805822586142b
Vasyl Dudka <dudka.vasia2010@gmail.com>
2024-12-14 13:41:52 +0200

    create card placeholder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,6 @@
-import { Component, ElementRef, viewChild } from '@angular/core';
-import { toObservable, toSignal } from '@angular/core/rxjs-interop';
-import { MatButton, MatButtonModule } from '@angular/material/button';
-import { filter, first, fromEvent, map, switchMap } from 'rxjs';
-import { DistortionCardComponent } from "../components/undefined/distortion-card/distortion-card.component";
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { DistortionCardComponent } from "../components/distortion-card/distortion-card.component";
 
 @Component({
   selector: 'app-root',
@@ -10,20 +8,4 @@ import { DistortionCardComponent } from "../components/undefined/distortion-card
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'username';
-
-  btnRef = viewChild<MatButton, ElementRef<HTMLButtonElement>>('btn', { read: ElementRef });
-  dominanceAsserted = this.getData();
-
-  private getData() {
-    const dataSource = toObservable(this.btnRef).pipe(
-      filter(ref => !!ref),
-      switchMap((ref) => fromEvent(ref.nativeElement, 'click')),
-      first(),
-      map(() => 'TRUE' as 'TRUE' | 'FALSE')
-    );
-
-    return toSignal(dataSource, { initialValue: 'FALSE' });
-  }
-}
+export class AppComponent {}
